aefc4820d381979d9955a275874f1b5e132f0746
rchen5884 <148903646+rchen5884@users.noreply.github.com>
2025-03-10 22:39:50 +0000

    chal

--- src/main/java/com/example/project/Encryptor.java
+++ src/main/java/com/example/project/Encryptor.java
@@ -3,21 +3,78 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Encryptor {
+    public static void main(String[] args) {
+        String message = "";
+        generateEncryptArray(message, 2);
+    }
     
-    public static int determineColumns(int messageLen, int rows){
-        
-        return 0;
+    public static int determineColumns(int messageLen, int rows) {
+        if (messageLen == 0) {
+            return 1;
+        }
+        return (int) Math.ceil((float) messageLen/rows);
     }
     
     public static String[][] generateEncryptArray(String message, int rows) {
-        return new String [0][0];
+        int messageLength = message.length();
+        int columns = determineColumns(messageLength, rows);
+        String[][] encryptArray = new String[rows][columns];
+
+        int idx = -1;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                idx++;
+                if (idx > messageLength - 1) {encryptArray[i][j] = "=";}
+                else {encryptArray[i][j] = message.substring(idx,idx+1);}
+            }
+        }
+
+        return encryptArray;
     }
 
     public static String encryptMessage(String message, int rows){
-        return "";
+        String[][] encryptArray = generateEncryptArray(message, rows);
+        int columns = determineColumns(message.length(), rows);
+        String newMessage = "";
+
+        for (int i = columns - 1; i >= 0; i--) {
+            for (int j = 0; j < rows; j++) {
+                newMessage += encryptArray[j][i];
+            }
+        }
+
+        return newMessage;
     }
 
     public static String decryptMessage(String encryptedMessage, int rows) {
-        return "";
+        int messageLength = encryptedMessage.length();
+        int columns = determineColumns(messageLength, rows);
+        String[][] decryptArray = new String[rows][columns];
+        int idx = 0;
+    
+        for (int i = columns - 1; i >= 0; i--) {
+            for (int j = 0; j < rows; j++) {
+                if (idx < messageLength) {
+                    decryptArray[j][i] = encryptedMessage.substring(idx, idx + 1);
+                    idx++;
+                } else {
+                    decryptArray[j][i] = "=";
+                }
+            }
+        }
+    
+        String originalMessage = "";
+    
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (!decryptArray[i][j].equals("=")) {
+                    originalMessage += decryptArray[i][j];
+                }
+            }
+        }
+        return originalMessage;
     }
+    
+    
 }
